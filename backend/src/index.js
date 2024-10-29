7bf0af8051adcbd0c628189ac97282e33819a373
oversanzero <oversanzero@gmail.com>
2024-10-29 22:13:37 +0000

    (backend) Add logger middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import indexRouter from "./controllers/indexController.js";
 import notesRouter from "./controllers/notesController.js";
+import logger from "./middlewares/logger.js";
 
 const app = express();
 const port = 3001;
 
 app.use(express.json());
+app.use(logger);
 app.use(`/`, indexRouter);
 app.use(`/`, notesRouter);
 
